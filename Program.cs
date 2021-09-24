05b9f552b3f708b76a3d3168c9881cff599fad30
Topzombie22 <90637885+Topzombie22@users.noreply.github.com>
2021-09-24 15:50:03 -0300

    V0.2 (Stats Added)

--- Program.cs
+++ Program.cs
@@ -6,11 +6,38 @@ namespace GithubConfirmation
     {
         static void Main(string[] args)
         {
+            string Title;
+            Title = "Dendrodome";
+
+            int lives;
+            lives = 3;
+
+            float exp;
+            exp = 0.0f;
+
+            int health;
+            health = 5;
+
+            int expMultiplyer;
+            expMultiplyer = 2;
+
+            int level;
+            level = 1;
+
+            string Role;
+            Role = "Healer";
+
+            string Race;
+            Race = "Human";
+
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
             Console.WriteLine("Are you allowed to play?");
             Console.ReadKey();
+
+
         }
     }
 }
